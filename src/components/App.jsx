1d87b7cded6b8cbc43eb36ff97a6e2f66959d34f
Kyryl2 <krasiuk.4884@gmail.com>
2024-05-05 23:13:12 +0200

    add forms

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,15 @@
-// src/components/App.jsx
+import PetFormFormik from './Forms/Form-Formik';
+import PetFormRHF from './Forms/Form-RHF';
+import PetForm from './Forms/Form-Usuall';
 
-import { Product } from './Product';
-
-export default function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Best selling</h1>
-
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
-    </div>
+    <>
+      <PetForm />
+      <PetFormFormik />
+      <PetFormRHF />
+    </>
   );
-}
+};
+
+export default App;
